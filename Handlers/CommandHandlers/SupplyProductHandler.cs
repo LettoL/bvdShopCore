2c48bb01247abae0e91635904b51dec6a900d49c
Denis <letto-l@yandex.ru>
2020-05-28 10:53:28 +0300

    supply product handler

---
+++ Handlers/CommandHandlers/SupplyProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Handlers.Commands;
+using PostgresData;
+
+namespace Handlers.CommandHandlers
+{
+    public static class SupplyProductHandler
+    {
+        public static async Task<int> Handle(this SupplyProduct command, PostgresContext db)
+        {
+            var currentDate = DateTime.UtcNow;
+            
+            var suppliedProduct = command.CreateSuppliedProduct(currentDate);
+
+            var suppliedProductSave = await db.SuppliedProducts.AddAsync(suppliedProduct);
+
+            await db.SaveChangesAsync();
+            
+            return suppliedProductSave.Entity.Id;
+        }
+    }
+}
---
+++ Handlers/Commands/SupplyProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Handlers.Commands
+{
+    public class SupplyProduct
+    {
+        public int ProductId { get; set; }
+        public int ShopId { get; set; }
+        public int SupplierId { get; set; }
+        public int Amount { get; set; }
+        public decimal ProcurementCost { get; set; }
+        public SuppliedType Type { get; set; }
+
+        public SuppliedProduct CreateSuppliedProduct(DateTime currentDate)
+        {
+            return new SuppliedProduct(
+                currentDate, Amount, ProcurementCost, ShopId, ProductId, Type, SupplierId);
+        }
+    }
+}
