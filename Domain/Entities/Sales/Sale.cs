50b6976974a65d2cfb2149635e3002bf26eeaa79
Denis <letto-l@yandex.ru>
2020-06-10 18:03:58 +0300

    domain sale

---
+++ Domain/Entities/Sales/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities.Sales
+{
+    public class Sale : Entity
+    {
+        public DateTime Date { get; private set; }
+
+        public decimal Sum { get; private set; }
+
+        public ICollection<SoldProduct> SoldProducts { get; private set; } = new HashSet<SoldProduct>();
+    }
+}
---
+++ Domain/Entities/Sales/SoldProduct.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities.Sales
+{
+    public class SoldProduct : Entity
+    {
+        public int ProductId { get; private set; }
+        public Product Product { get; private set; }
+
+        public int SaleId { get; private set; }
+        public Sale Sale { get; private set; }
+
+        public int Amount { get; private set; }
+
+        public SoldProduct(int productId, int saleId, int amount) =>
+            (ProductId, SaleId, Amount) = (productId, saleId, amount);
+
+        public SoldProduct(int productId, Sale sale, int amount) =>
+            (ProductId, Sale, Amount) = (productId, sale, amount);
+    }
+}
